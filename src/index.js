f7cbb318ec2c81f755ea533eeebcff8d54f3d5ed
Maxoverking <avemax7@gmail.com>
2023-05-23 15:47:33 +0200

    try fix 404

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import "./index.css";
-import React, { Suspense } from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { store, persistor } from "./redux/store";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import App from "./App";
-import Loader from "./component/Loader/Loader";
+import './index.css';
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { store, persistor } from './redux/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import App from './App';
+import Loader from './component/Loader/Loader';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
-    <BrowserRouter basename="test-task">
+    <BrowserRouter basename="test-task/">
       <PersistGate loading={null} persistor={persistor}>
         <Suspense fallback={<Loader />}>
           <App />
